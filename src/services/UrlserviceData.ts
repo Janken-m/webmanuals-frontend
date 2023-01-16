1bcc24ef4a4f8162ee543d0d89c4c68f7febeb0f
Janken-m <jankenmatene933@gmail.com>
2023-01-16 21:05:18 +0100

    deploy url

--- src/services/UrlserviceData.ts
+++ src/services/UrlserviceData.ts
@@ -3,7 +3,9 @@ import { toast } from "react-toastify";
 
 export const handleSingleRequest = async (shortenId: string) => {
   await axios
-    .get(`http://localhost:5000/api/shorten/${shortenId}`)
+    .get(
+      `https://webmanuals-arbetsprov-api.onrender.com/api/shorten/${shortenId}`
+    )
     .then((res) => window.open(res.data))
     .catch((error) => {
       toast.error(error.response.data.error);
--- src/store/Api.ts
+++ src/store/Api.ts
@@ -1,7 +1,9 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const Api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://webmanuals-arbetsprov-api.onrender.com/api",
+  }),
   tagTypes: ["shorten"],
   endpoints: (builder) => ({
     getUrls: builder.query({
